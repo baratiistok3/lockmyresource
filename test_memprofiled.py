d6c17a3b95b39f36de262077e9cf186a065bb293
Szabó Péter <1254135+szabopeter@users.noreply.github.com>
2021-04-01 14:05:19 +0200

    Prepare for memory profiling

---
+++ test_memprofiled.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+from unittest.mock import patch
+import util
+
+
+class ProfileMock:
+    def __init__(self):
+        self.results = []
+    def profile(self, func):
+        def inner(*args, **kwargs):
+            result = func(*args, **kwargs)
+            self.results.append(result)
+            return result
+        return inner
+
+
+profile_mock_state = ProfileMock()
+profile_mock = profile_mock_state.profile
+
+
+def dummy_decorator(func):
+    return func
+
+
+class MemprofiledTestCase(TestCase):
+    @patch("util.memprofiled", dummy_decorator)
+    def test_without_profiler(self):
+        @util.memprofiled
+        def call_target(a: int, b:int) -> int:
+            return a + b
+
+        self.assertEqual(5, call_target(2, 3))
+    
+    @patch("util.memprofiled", profile_mock)
+    def test_with_profiler(self):
+        @util.memprofiled
+        def call_target(a: int, b:int) -> int:
+            return a + b
+
+        self.assertEqual(5, call_target(2, 3))
+        self.assertEqual([5], profile_mock_state.results)
--- util.py
+++ util.py
@@ -1,6 +1,16 @@
 import logging
 
 
+try:
+    from memory_profiler import profile
+    memprofiled = profile
+    logging.warning("Using memory_profiler")
+except ImportError:
+    def memprofiled(func):
+        return func
+    logging.info("Not using memory profiler")
+
+
 def traced(func):
     def inner(*args, **kwargs):
         logging.debug("%s(%s, %s)", func.__name__, args, kwargs)
@@ -9,3 +19,14 @@ def traced(func):
         return result
 
     return inner
+
+
+def memprofiled2(func):
+    if profile is None:
+        return func
+
+    # def inner(*args, **kwargs):
+    #     result = func(*args, **kwargs)
+    #     return result    
+
+    return profile(func)
