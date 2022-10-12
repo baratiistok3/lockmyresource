9a2f3fff23ca8fd08e80706b406fe15de72cdbdf
Barati <istvan.barati@evosoft.com>
2022-10-12 18:46:21 +0200

    use display name on windows instead of username

--- lockmyresource/core.py
+++ lockmyresource/core.py
@@ -2,6 +2,9 @@ import datetime
 import logging
 import os
 import sqlite3
+import sys
+if sys.platform.startswith("win"):
+    import ctypes
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from pathlib import Path
@@ -39,9 +42,24 @@ class User:
     @staticmethod
     def from_os() -> "User":
         try:
+            if sys.platform.startswith("win"):
+                return User(User.get_display_name())
             return User(os.getlogin())
         except OSError:
             return no_user
+
+    if sys.platform.startswith("win"):
+        @staticmethod
+        def get_display_name():
+            get_user_name_ex = ctypes.windll.secur32.GetUserNameExW
+            name_display = 3
+
+            size = ctypes.pointer(ctypes.c_ulong(0))
+            get_user_name_ex(name_display, None, size)
+
+            name_buffer = ctypes.create_unicode_buffer(size.contents.value)
+            get_user_name_ex(name_display, name_buffer, size)
+            return name_buffer.value
 
 
 no_user = User(None)
