df73e381b57a214741d2da2fae3bc90e94a232ae
Szabó Péter <1254135+szabopeter@users.noreply.github.com>
2022-10-17 08:32:36 +0200

    Describe User equality behavior with unit test

---
+++ tests/test_user.py
@@ -0,0 +1,12 @@
+import unittest
+
+from core import User, no_user
+
+
+class UserTestCase(unittest.TestCase):
+    def test_equality(self):
+        self.assertEqual(User(None), User(None))
+    
+    def test_same(self):
+        self.assertEqual(no_user, User(None))
+        self.assertIsNot(User(None), no_user)
